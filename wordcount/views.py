efe01dbefbd418e5592cdd08df43dbf2c324d97b
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-13 18:16:03 -0600

    about me + frequency table sorted by most frequent word

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from langdetect import detect
+from collections import Counter
 
+# GENERAL
 def home(request):
     '''
     Renders home page with form to receive user input
     '''
     return render(request, 'home.html')
 
+
+def about(request):
+    '''
+    Render "about me" page
+    '''
+    return render(request, 'about.html')
+
+
+# FUNCTIONALITY
 def result(request):
     '''
     Counts number of words & detects which language input used
@@ -15,12 +26,16 @@ def result(request):
     # grabs entire user input from textarea "fulltext"
     fulltext = request.GET['fulltext']
 
+    frequency = dict(Counter(fulltext.split()))
+
+    sorted_frequency = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+
     # splits the string into array and counts its lenght
     count = len(fulltext.split())
     
     # detects the language in the string
     language = detect(fulltext)
 
-    context = {'fulltext': fulltext, 'count': count, 'language': language}
+    context = {'fulltext': fulltext, 'count': count, 'language': language, 'sorted_frequency': sorted_frequency}
 
     return render(request, 'result.html', context)
